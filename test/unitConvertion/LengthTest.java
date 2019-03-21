2bc1795e3fd67d7a89d189b79066244a1b53b29a
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-03-21 23:42:26 +0530

    use Quntity interface to store instance of Length and Volume class

--- test/unitConvertion/LengthTest.java
+++ test/unitConvertion/LengthTest.java
@@ -11,78 +11,78 @@ class LengthTest {
 
   @Test
   void shouldReturnTrueIfTwoFeetAreEqual() {
-    Length feet1 = new Length(new BigDecimal(1), Length.LengthUnits.FEET);
-    Length feet2 = new Length(new BigDecimal(1), Length.LengthUnits.FEET);
+    Quantity feet1 = new Length(new BigDecimal(1), Length.LengthUnits.FEET);
+    Quantity feet2 = new Length(new BigDecimal(1), Length.LengthUnits.FEET);
     assertEquals(feet1, feet2);
   }
 
   @Test
   void shouldReturnFalseIfTwoFeetAreNotEqual() {
-    Length twoFeet = new Length(new BigDecimal(2), Length.LengthUnits.FEET);
-    Length twelveFeet = new Length(new BigDecimal(12), Length.LengthUnits.FEET);
+    Quantity twoFeet = new Length(new BigDecimal(2), Length.LengthUnits.FEET);
+    Quantity twelveFeet = new Length(new BigDecimal(12), Length.LengthUnits.FEET);
     assertNotEquals(twoFeet, twelveFeet);
   }
 
   @Test
   void shouldReturnTrueIfTwoInchAreEqual() {
-    Length inch1 = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
-    Length inch2 = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
+    Quantity inch1 = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
+    Quantity inch2 = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
     assertEquals(inch1, inch2);
   }
 
   @Test
   void shouldReturnFalseIfTwoInchesAreNotEqual() {
-    Length twoInches = new Length(new BigDecimal(2), Length.LengthUnits.INCH);
-    Length twelveInches = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
+    Quantity twoInches = new Length(new BigDecimal(2), Length.LengthUnits.INCH);
+    Quantity twelveInches = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
     assertNotEquals(twoInches, twelveInches);
   }
 
   @Test
   void shouldReturnTrueIfFeetIsEqualToInch() {
-    Length oneFeet = new Length(new BigDecimal(1), Length.LengthUnits.FEET);
-    Length twelveInches = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
+    Quantity oneFeet = new Length(new BigDecimal(1), Length.LengthUnits.FEET);
+    Quantity twelveInches = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
     assertEquals(oneFeet, twelveInches);
   }
 
   @Test
   void shouldReturnFalseIfFeetIsNotEqualToInch() {
-    Length twoFeet = new Length(new BigDecimal(2), Length.LengthUnits.FEET);
-    Length twelveInches = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
+    Quantity twoFeet = new Length(new BigDecimal(2), Length.LengthUnits.FEET);
+    Quantity twelveInches = new Length(new BigDecimal(12), Length.LengthUnits.INCH);
     assertNotEquals(twoFeet, twelveInches);
   }
 
   @Test
   void shouldReturnTrueIfInchAndCmAreEqual() {
-    Length fiveCm = new Length(new BigDecimal(5), Length.LengthUnits.CM);
-    Length twoInches = new Length(new BigDecimal(2), Length.LengthUnits.INCH);
+    Quantity fiveCm = new Length(new BigDecimal(5), Length.LengthUnits.CM);
+    Quantity twoInches = new Length(new BigDecimal(2), Length.LengthUnits.INCH);
     assertEquals(fiveCm, twoInches);
   }
 
   @Test
   void shouldReturnFalseIfInchAndCmAreNotEqual() {
-    Length fiveCm = new Length(new BigDecimal(5), Length.LengthUnits.CM);
-    Length twoInches = new Length(new BigDecimal(3), Length.LengthUnits.INCH);
+    Quantity fiveCm = new Length(new BigDecimal(5), Length.LengthUnits.CM);
+    Quantity twoInches = new Length(new BigDecimal(3), Length.LengthUnits.INCH);
     assertNotEquals(fiveCm, twoInches);
   }
 
   @Test
   void shouldReturnTrueIfCmAndMmAreEqual() {
-    Length fiveCm = new Length(new BigDecimal(2), Length.LengthUnits.CM);
-    Length twoInches = new Length(new BigDecimal(20), Length.LengthUnits.MM);
+    Quantity fiveCm = new Length(new BigDecimal(2), Length.LengthUnits.CM);
+    Quantity twoInches = new Length(new BigDecimal(20), Length.LengthUnits.MM);
     assertEquals(fiveCm, twoInches);
   }
 
   @Test
   void shouldReturnFalseIfCmAndMmAreNotEqual() {
-    Length fiveCm = new Length(new BigDecimal(5), Length.LengthUnits.CM);
-    Length twoInches = new Length(new BigDecimal(20), Length.LengthUnits.INCH);
+    Quantity fiveCm = new Length(new BigDecimal(5), Length.LengthUnits.CM);
+    Quantity twoInches = new Length(new BigDecimal(20), Length.LengthUnits.INCH);
     assertNotEquals(fiveCm, twoInches);
   }
 
   @Test
   void shouldReturnTrueIfFeetAndMmAreEqual() {
-    Length fiveCm = new Length(new BigDecimal(300), Length.LengthUnits.MM);
-    Length twoInches = new Length(new BigDecimal(1), Length.LengthUnits.FEET);
+    Quantity fiveCm = new Length(new BigDecimal(300), Length.LengthUnits.MM);
+    Quantity twoInches = new Length(new BigDecimal(1), Length.LengthUnits.FEET);
     assertEquals(fiveCm, twoInches);
   }
 
--- test/unitConvertion/VolumeTest.java
+++ test/unitConvertion/VolumeTest.java
@@ -11,43 +11,43 @@ class VolumeTest {
 
   @Test
   void shouldReturnTrueIfTwoGallonsAreEqual() {
-    Volume oneGallon = new Volume(new BigDecimal(1), Volume.VolumeUnits.GALLON);
-    Volume liter = new Volume(new BigDecimal(1), Volume.VolumeUnits.GALLON);
+    Quantity oneGallon = new Volume(new BigDecimal(1), Volume.VolumeUnits.GALLON);
+    Quantity liter = new Volume(new BigDecimal(1), Volume.VolumeUnits.GALLON);
     assertEquals(oneGallon, liter);
   }
 
   @Test
   void shouldReturnFalseIfTwoGallonsAreNotEqual() {
-    Volume oneGallon = new Volume(new BigDecimal(2), Volume.VolumeUnits.GALLON);
-    Volume liter = new Volume(new BigDecimal(0.26), Volume.VolumeUnits.GALLON);
+    Quantity oneGallon = new Volume(new BigDecimal(2), Volume.VolumeUnits.GALLON);
+    Quantity liter = new Volume(new BigDecimal(0.26), Volume.VolumeUnits.GALLON);
     assertNotEquals(oneGallon, liter);
   }
 
   @Test
   void shouldReturnFalseIfTwoLitersAreNotEqual() {
-    Volume oneGallon = new Volume(new BigDecimal(2), Volume.VolumeUnits.LITER);
-    Volume liter = new Volume(new BigDecimal(0.26), Volume.VolumeUnits.LITER);
+    Quantity oneGallon = new Volume(new BigDecimal(2), Volume.VolumeUnits.LITER);
+    Quantity liter = new Volume(new BigDecimal(0.26), Volume.VolumeUnits.LITER);
     assertNotEquals(oneGallon, liter);
   }
 
   @Test
   void shouldReturnTrueIfTwoLiterAreEqual() {
-    Volume oneGallon = new Volume(new BigDecimal(2), Volume.VolumeUnits.LITER);
-    Volume liter = new Volume(new BigDecimal(0.26), Volume.VolumeUnits.LITER);
+    Quantity oneGallon = new Volume(new BigDecimal(2), Volume.VolumeUnits.LITER);
+    Quantity liter = new Volume(new BigDecimal(0.26), Volume.VolumeUnits.LITER);
     assertNotEquals(oneGallon, liter);
   }
 
   @Test
   void shouldReturnTrueIfGallonAndLiterAreEqual() {
-    Volume oneGallon = new Volume(new BigDecimal(1), Volume.VolumeUnits.GALLON);
-    Volume liter = new Volume(new BigDecimal(0.264172), Volume.VolumeUnits.LITER);
+    Quantity oneGallon = new Volume(new BigDecimal(1), Volume.VolumeUnits.GALLON);
+    Quantity liter = new Volume(new BigDecimal(0.264172), Volume.VolumeUnits.LITER);
     assertEquals(oneGallon, liter);
   }
 
   @Test
   void shouldReturnFalseIfGallonAndLiterAreNotEqual() {
-    Volume oneGallon = new Volume(new BigDecimal(2), Volume.VolumeUnits.GALLON);
-    Volume liter = new Volume(new BigDecimal(0.26), Volume.VolumeUnits.LITER);
+    Quantity oneGallon = new Volume(new BigDecimal(2), Volume.VolumeUnits.GALLON);
+    Quantity liter = new Volume(new BigDecimal(0.26), Volume.VolumeUnits.LITER);
     assertNotEquals(oneGallon, liter);
   }
 }
